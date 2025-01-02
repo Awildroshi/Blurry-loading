e56d9f883fcca3d963b322b35fdb488b6c8f5857
Awildroshi <151027830+Awildroshi@users.noreply.github.com>
2025-01-02 23:35:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,8 +13,6 @@ function blurr(){
     console.log('load');
     text.style.opacity= scale(load, 0 ,100, 1 ,0);
     bg.style.filter= `blur(${scale(load, 0, 100, 30, 0)}px)`;
-
-
 }
 function scale (number, inMin, inMax, outMin, outMax) {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
